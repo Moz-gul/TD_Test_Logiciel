052a6226bcb42dd57a7ff97571f5e6fc8f44b852
Gabriel POGGIA <gabriel.poggia@etu.sorbonne-universite.fr>
2024-01-08 13:18:27 +0100

    add three_max_int tests and success

--- ex1.py
+++ ex1.py
@@ -1,3 +1,23 @@
 # Implementation 1
 def three_max_int(list):
-    pass
+
+    if (len(list) >= 3):
+
+        three_max = [list[0], 0, 0]
+
+        for i in range (1, len(list)):
+            if (list[i] > three_max[0]):
+                three_max[2] = three_max[1]
+                three_max[1] = three_max[0]
+                three_max[0] = list[i]
+            elif (list[i] > three_max[1]):
+                three_max[2] = three_max[1]
+                three_max[1] = list[i]
+            elif (list[i] > three_max[2]):
+                three_max[2] = list[i]
+        
+        return three_max
+    
+    else:
+        print("Erreur : la liste est trop petite...")
+        return None
