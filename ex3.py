254b609b8b7f445dbbd7408bf92a368926f10c81
jky-chen <zoroyamamoto6@gmail.com>
2024-01-11 16:55:22 +0100

    apply YAGNI method

--- ex3.py
+++ ex3.py
@@ -8,12 +8,12 @@ def rms(d):
 def initial_rms_norm(d):
     s_norm = np.zeros(d.shape, dtype=complex)
     for i in range(d.shape[0]):
-        sig_amplitude = np.abs(d[i])
-        rms = np.sqrt(np.mean(sig_amplitude ** 2))
+        rms = np.sqrt(np.mean(np.abs(d[i]) ** 2))
         s_norm[i] = d[i] / rms
     return s_norm.tolist()
 
-def new_rms_norm(d, desired_rms=1.0):
+def new_rms_norm(d):
     # Calculate the RMS normalization factor
-    a = np.sqrt( len(d) * desired_rms**2 / sum(np.abs(d)**2))
+    # desired_rms=1.0
+    a = np.sqrt( len(d) / sum(np.abs(d)**2))
     return [ d_k * a for d_k in d]
