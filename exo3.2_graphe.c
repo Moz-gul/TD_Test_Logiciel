9ca5b51b27523a668da343f43b46df859cdab5ac
jky-chen <zoroyamamoto6@gmail.com>
2024-01-13 00:09:40 +0100

    Initial commit of the original code for ex3, about graphs

---
+++ exo3.2_graphe.c
@@ -0,0 +1,215 @@
+#include "graphe.h"
+
+// CREATION DE GRAPHE
+t_graphe * creer_graphe(int nb_sommets)
+{
+    // Allocation dynamique du graphe et de sa matrice d'adjacences
+    t_graphe * G = (t_graphe*) malloc(sizeof(t_graphe));
+    if(G == NULL)
+    {
+        printf("Echec de la creation de graphe\n");
+        exit(EXIT_FAILURE);
+    }
+    G->matrice = (int**) malloc(nb_sommets*sizeof(int*));
+    if(G->matrice == NULL)
+    {
+        printf("Echec de la creation de graphe\n");
+        exit(EXIT_FAILURE);
+    }
+    for(int i=0 ; i<nb_sommets ; i++)
+    {
+        G->matrice[i] = (int*) malloc(nb_sommets*sizeof(int));
+        if(G->matrice[i] == NULL)
+        {
+            printf("Echec de la creation de graphe\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    // Initialisation du graphe et de sa matrice, "l" designant la ligne et "c" la colonne
+    G->nb_sommets = nb_sommets;
+    for(int l=0 ; l<nb_sommets ; l++)
+        for(int c=0 ; c<nb_sommets ; c++) G->matrice[l][c] = 0;
+
+    return G;
+}
+
+
+// CREATION ALEATOIRE DE GRAPHE
+t_graphe * creer_graphe_aleatoirement(void)
+{
+    srand(time(NULL));
+
+    // Dimensionnement aleatoire du graphe
+    int nb_sommets = (rand() % (NB_SOMMETS_MAX-1)) + 2;  // entre 2 et NB_SOMMETS_MAX sommets
+    t_graphe * G = creer_graphe(nb_sommets);
+
+    // Ajout aleatoire d'adjacences
+    int nb_adjecences = (rand() % nb_sommets*2) + 5;    // entre 5 et nb_sommets*2 + 5 ajout d'adjacences
+    int src, dest;
+    bool estOriente;
+    if(rand() % 2 == 0) estOriente = true;
+    else estOriente = false;
+
+    for(int i=0 ; i<nb_adjecences ; i++)
+    {
+        do{
+            src = rand() % nb_sommets;
+            dest = rand() % nb_sommets;
+        } while(src == dest);
+        ajouter_adjacence(G, src, dest, estOriente);
+    }
+
+    return G;
+}
+
+
+// LIBERATION DE GRAPHE
+void liberer_graphe(t_graphe * G)
+{
+    for(int l=0 ; l<G->nb_sommets ; l++) free(G->matrice[l]);
+    free(G->matrice);
+    free(G);
+}
+
+
+// AFFICHAGE DE LA MATRICE DU GRAPHE
+void afficher_graphe(t_graphe * G)
+{
+    printf("\nAffichage du graphe :\n\n%*s  ",3,"V");
+    for(int c=0 ; c<G->nb_sommets ; c++) printf("%*d",3,c);
+    printf("\n\n");
+
+    for(int l=0 ; l<G->nb_sommets ; l++)
+    {
+        printf("%*d  ",3,l);
+        for(int c=0 ; c<G->nb_sommets ; c++) printf("%*d",3,G->matrice[l][c]);
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+// AJOUTER UNE ADJACENCE
+void ajouter_adjacence(t_graphe * G, int src, int dest, bool estOriente)
+{
+    if((src < G->nb_sommets) && (dest < G->nb_sommets))
+    {
+        if(src != dest)
+        {
+            G->matrice[src][dest] = 1;
+            if(estOriente == false) G->matrice[dest][src] = 1;
+        }
+        else printf("Echec de l'ajout d'une adjacence : les sommets choisis ne doivent pas etre les memes\n");
+    }
+    else printf("Echec de l'ajout d'une adjacence : au moins un sommet n'existe pas\n");
+}
+
+
+// PARCOURS EN PROFONDEUR
+t_graphe * parcours_profondeur(t_graphe * G, int v)
+{
+    // Initialisation : creation de 2 tableaux
+    int * pere = (int*) malloc(G->nb_sommets * sizeof(int));
+    bool * estMarque = (bool*) malloc(G->nb_sommets * sizeof(bool));
+    for(int i=0 ; i<G->nb_sommets ; i++)
+    {
+        pere[i] = 1000; // nil
+        estMarque[i] = false;
+    }
+
+    // Exploration du graphe
+    DFS(G, v, pere, estMarque);
+
+    // Verification que tous les sommets aient ete explores
+    for(int i=0 ; i<G->nb_sommets ; i++)
+    {
+        // Si un sommet n'est pas marque, lancement d'un parcours a partir de ce dernier
+        if(estMarque[i] == false) DFS(G, i, pere, estMarque);
+    }
+
+    // Affichage du tableau pere[]
+    printf("\nTableau pere[] :\n\n%*s :",10,"sommets");
+    for(int i=0 ; i<G->nb_sommets ; i++) printf("%*d",3,i);
+    printf("\n%*s :",10,"peres");
+    for(int i=0 ; i<G->nb_sommets ; i++)
+    {
+        if(pere[i] != 1000) printf("%*d",3,pere[i]);
+        else printf("   ");
+    }
+    printf("\n\n");
+
+    // Creation de l'arborescence
+    t_graphe * A = creer_graphe(G->nb_sommets);
+    for(int i=0 ; i<G->nb_sommets ; i++)
+    {
+        if(pere[i] < 1000) A->matrice[pere[i]][i] = 1;
+    }
+
+    return A;
+}
+
+
+// DEPTH FIRST SEARCH
+void DFS(t_graphe * G, int v, int * pere, bool * estMarque)
+{
+    estMarque[v] = true;
+    for(int c=0 ; c<G->nb_sommets ; c++)
+    {
+        if(G->matrice[v][c] == 1)
+        {
+            if(estMarque[c] == false)
+            {
+                pere[c] = v;
+                DFS(G, c, pere, estMarque);
+            }
+        }
+    }
+}
+
+
+// SAUVEGARDER UN GRAPHE DANS UN FICHIER
+void sauver_graphe(t_graphe * G, char * nom_fichier)
+{
+    FILE * fichier = fopen(nom_fichier, "w");
+    if(fichier == NULL)
+    {
+        printf("Erreur d'ouverture de fichier\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(fichier, "%d\n", G->nb_sommets);
+    for(int l=0 ; l<G->nb_sommets ; l++)
+    {
+        for(int c=0 ; c<G->nb_sommets ; c++)
+        {
+            fprintf(fichier, " %d ", G->matrice[l][c]);
+        }
+        fprintf(fichier, "\n");
+    }
+
+    fclose(fichier);
+}
+
+
+// RECUPERER UN GRAPHE DEPUIS UN FICHIER
+t_graphe * charger_graphe(const char * nom_fichier)
+{
+    FILE * fichier = fopen(nom_fichier, "r");
+    if(fichier == NULL)
+    {
+        printf("Erreur d'ouverture de fichier\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int nb_sommets;
+    fscanf(fichier, "%d", &nb_sommets);
+    t_graphe * G = creer_graphe(nb_sommets);
+
+    for(int l=0 ; l<nb_sommets ; l++)
+        for(int c=0 ; c<nb_sommets ; c++) 
+            fscanf(fichier, "%d", &G->matrice[l][c]);
+
+    fclose(fichier);
+    return G;
+}
---
+++ exo3.2_graphe.h
@@ -0,0 +1,31 @@
+#ifndef __GRAPHE_H__
+#define __GRAPHE_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define NB_SOMMETS_MAX 20
+
+// Structure de données pour les graphes : matrices d'adjacences
+typedef struct
+{
+    int nb_sommets;
+    int ** matrice;
+} t_graphe;
+
+// Fonctions de base
+t_graphe * creer_graphe(int nb_sommets);
+t_graphe * creer_graphe_aleatoirement(void);
+void liberer_graphe(t_graphe * G);
+void afficher_graphe(t_graphe * G);
+void ajouter_adjacence(t_graphe * G, int src, int dest, bool estOriente);
+void sauver_graphe(t_graphe * G, char * nom_fichier);
+t_graphe * charger_graphe(const char * nom_fichier);
+
+// Algorithme de parcours en profondeur
+t_graphe * parcours_profondeur(t_graphe * G, int v);
+void DFS(t_graphe * G, int v, int * pere, bool * estMarque);
+
+#endif
